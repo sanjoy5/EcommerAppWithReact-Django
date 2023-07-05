84e1ad09769f878a5e6096f39f937e25bf812e62
Sanjoy Sarker <sarkerd52@gmail.com>
2023-07-06 01:00:14 +0600

    template not found

--- eshop/urls.py
+++ eshop/urls.py
@@ -1,17 +1,22 @@
 """eshop URL Configuration"""
 
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path,include,re_path
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 from django.conf import settings
 from django.conf.urls.static import static
 
+def home(request):
+    return HttpResponse('RDSHOP Running Succefully')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/login/',obtain_auth_token),
-    path('',TemplateView.as_view(template_name='index.html')),
+    # path('',TemplateView.as_view(template_name='index.html')),
+    path('',home),
     path('api/',include('shop.urls')),
 ]
 
