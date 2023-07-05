c8b88ed45e8c33e4a5eb8224e6b31341b66f0261
Sanjoy Sarker <sarkerd52@gmail.com>
2023-07-06 00:56:31 +0600

    same issue solve

--- eshop/settings.py
+++ eshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,7 +141,7 @@ MEDIA_URL = 'media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
-STATIC_ROOT = BASE_DIR / 'frontend/build/staticroot'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
