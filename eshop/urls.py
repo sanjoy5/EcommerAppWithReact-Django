4d36d13e9b3d672c3b3089ffb3dc68e8d9b8ab47
Sanjoy Sarker <sarkerd52@gmail.com>
2023-07-06 01:03:59 +0600

    template issue solve

--- eshop/urls.py
+++ eshop/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += [
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-]
+# urlpatterns += [
+#     re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+# ]
